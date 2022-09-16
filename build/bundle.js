886e886e33401fbdd9a1e8cb0cf7b6440d4d2838
Diogo Manhaes <dsmanhaes@gmail.com>
2022-09-16 11:18:28 -0300

    Typescript sample

---
+++ build/bundle.js
@@ -0,0 +1,8 @@
+var idade = 27;
+var nome = 'Diogo';
+var maiorDeIdade = true;
+function soma(num1, num2) {
+    var soma = num1 + num2;
+    return soma;
+}
+document.write("A soma de 7 + 5 \u00E9 ".concat(soma(7, 5)));
